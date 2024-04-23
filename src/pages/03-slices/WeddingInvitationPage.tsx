09937baafd7d23a66d0b7378c63c73170f3926cf
Jhon Moofarry <jhon.moofarry00@gmail.com>
2024-04-22 23:02:18 -0500

    slices pattern

--- src/pages/03-slices/WeddingInvitationPage.tsx
+++ src/pages/03-slices/WeddingInvitationPage.tsx
@@ -1,8 +1,42 @@
 import { WhiteCard } from '../../components';
+import { useWeddingBoundStore } from '../../stores/wedding';
 
 
 
 export const WeddingInvitationPage = () => {
+
+  const firstName = useWeddingBoundStore(state => state.firstName);
+  const lastName = useWeddingBoundStore(state => state.lastName);
+  const setFirstName = useWeddingBoundStore(state => state.setFirstName);
+  const setLastName = useWeddingBoundStore(state => state.setLastName);
+
+  const guestCount = useWeddingBoundStore(state => state.guestCount);
+  const setGuestCount = useWeddingBoundStore(state => state.setGuestCount);
+
+  const eventYYYYMMDD = useWeddingBoundStore(state => state.eventYYYYMMDD());
+  const eventHHMM = useWeddingBoundStore(state => state.eventHHMM());
+  const setEventDate = useWeddingBoundStore(state => state.setEventDate);
+  const setEventTime = useWeddingBoundStore(state => state.setEventTime);
+
+
+  const isConfirmed = useWeddingBoundStore(state => state.isConfirmed);
+  const setIsConfirmed = useWeddingBoundStore(state => state.setIsConfirmed);
+
+  const eventDate = useWeddingBoundStore(state => state.eventDate);
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+
+    event.preventDefault();
+    console.log({
+      firstName,
+      lastName,
+      guestCount,
+      eventDate,
+      isConfirmed
+    });
+  }
+
+
   return (
     <>
       <h1>Invitación de Boda</h1>
@@ -11,7 +45,7 @@ export const WeddingInvitationPage = () => {
 
       <WhiteCard className="flex items-center justify-center p-12">
         <div className="mx-auto w-full max-w-[550px]">
-          <form>
+          <form onSubmit={onSubmit}>
             <div className="-mx-3 flex flex-wrap">
               <div className="w-full px-3 sm:w-1/2">
                 <div className="mb-5">
@@ -25,6 +59,8 @@ export const WeddingInvitationPage = () => {
                     name="firstName"
                     id="firstName"
                     placeholder="Primer Nombre"
+                    value={firstName}
+                    onChange={({ target }) => setFirstName(target.value)}
                   />
                 </div>
               </div>
@@ -40,6 +76,8 @@ export const WeddingInvitationPage = () => {
                     name="lastName"
                     id="lastName"
                     placeholder="Apellido"
+                    value={lastName}
+                    onChange={({ target }) => setLastName(target.value)}
                   />
                 </div>
               </div>
@@ -57,6 +95,8 @@ export const WeddingInvitationPage = () => {
                 placeholder="5"
                 min="0"
                 className="w-full appearance-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                value={guestCount}
+                onChange={({ target }) => setGuestCount(+(target.value))}
               />
             </div>
 
@@ -72,6 +112,8 @@ export const WeddingInvitationPage = () => {
                     type="date"
                     name="eventDate"
                     id="eventDate"
+                    value={eventYYYYMMDD}
+                    onChange={({ target }) => setEventDate(target.value)}
                   />
                 </div>
               </div>
@@ -86,6 +128,9 @@ export const WeddingInvitationPage = () => {
                     type="time"
                     name="eventTime"
                     id="eventTime"
+                    value={eventHHMM}
+                    onChange={({ target }) => setEventTime(target.value)}
+
                   />
                 </div>
               </div>
@@ -102,6 +147,8 @@ export const WeddingInvitationPage = () => {
                     name="isComing"
                     id="radioButton1"
                     className="h-5 w-5"
+                    checked={isConfirmed}
+                    onChange={() => setIsConfirmed(true)}
                   />
                   <label
                     className="pl-3 text-base font-medium text-[#07074D]"
@@ -115,6 +162,9 @@ export const WeddingInvitationPage = () => {
                     name="isComing"
                     id="radioButton2"
                     className="h-5 w-5"
+                    checked={!isConfirmed}
+                    onChange={() => setIsConfirmed(false)}
+
                   />
                   <label
                     className="pl-3 text-base font-medium text-[#07074D]"
---
+++ src/stores/wedding/confirmation.slice.ts
@@ -0,0 +1,11 @@
+import { StateCreator } from 'zustand';
+
+export interface ConfirmationSlice {
+  isConfirmed: boolean;
+  setIsConfirmed: (isConfirmed: boolean) => void;
+}
+
+export const createConfirmationSlice: StateCreator<ConfirmationSlice> = set => ({
+  isConfirmed: false,
+  setIsConfirmed: (isConfirmed: boolean) => set({ isConfirmed })
+});
